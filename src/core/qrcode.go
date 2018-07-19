6ef0dd92c03b5326f5aaf2d12732c8a15ce28f75
keng <keng@undesoft.com>
2018-07-19 19:56:28 +0800

    裁剪二维码。

--- src/core/qrcode.go
+++ src/core/qrcode.go
@@ -1,17 +1,32 @@
 package core
 
 import (
+	"bytes"
 	"encoding/base64"
+	"image"
+	"image/png"
 
 	qrcode "github.com/skip2/go-qrcode"
 )
 
 // CreateQrcodeBase64 创建二维码
 func CreateQrcodeBase64(s string) (string, error) {
-	var png []byte
-	png, err := qrcode.Encode(s, qrcode.Medium, 256)
+	var pngQrcode []byte
+	pngQrcode, err := qrcode.Encode(s, qrcode.Medium, 256)
 	if err != nil {
 		return "", err
 	}
-	return "data:image/png;base64," + base64.StdEncoding.EncodeToString(png), nil
+
+	pngBuffer := bytes.NewBuffer(pngQrcode)
+	// 图片文件解码
+	img, _, err := image.Decode(pngBuffer)
+	if err != nil {
+		return "", err
+	}
+	rgbImg := img.(*image.Paletted)
+	//图片裁剪x0 y0 x1 y1
+	subImg := rgbImg.SubImage(image.Rect(35, 35, 220, 220)).(*image.Paletted)
+	emptyBuffer := bytes.NewBuffer(nil)
+	png.Encode(emptyBuffer, subImg)
+	return "data:image/png;base64," + base64.StdEncoding.EncodeToString(emptyBuffer.Bytes()), nil
 }
